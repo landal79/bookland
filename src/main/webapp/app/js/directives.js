a6efd5ea5170173b570ec5ca3b5571376106261e
alexlan <alex.landini@gmail.com>
2015-03-07 14:32:25 +0100

    refactoring to directives book tags

--- src/main/webapp/app/js/directives.js
+++ src/main/webapp/app/js/directives.js
@@ -1,42 +1,99 @@
 'use strict';
 
-var directives = angular.module('bookland.directives', [ 'ngResource' ]);
+var directives = angular.module('bookland.directives', [ 'ngResource',
+    'bookland.services' ]);
 
 directives.directive('fileModel', [ '$parse', function($parse) {
-	return {
-		restrict : 'A',
-		link : function(scope, element, attrs) {
-			var model = $parse(attrs.fileModel);
-			var modelSetter = model.assign;
+  return {
+    restrict : 'A',
+    link : function(scope, element, attrs) {
+      var model = $parse(attrs.fileModel);
+      var modelSetter = model.assign;
 
-			element.bind('change', function() {
-				scope.$apply(function() {
-					modelSetter(scope, element[0].files[0]);
-				});
-			});
-		}
+      element.bind('change', function() {
+        scope.$apply(function() {
+          modelSetter(scope, element[0].files[0]);
+        });
+      });
+    }
 
-	};
+  };
 } ]);
 
 directives.directive('uploadBtn', [ function() {
-	return {
-		restrict : 'C',
-		link : function(scope, elem, attrs) {
-			var inputFile = elem.parent().find('input')[0];
-			elem.bind('click', function() {
-				inputFile.click();
-			});
+  return {
+    restrict : 'C',
+    link : function(scope, elem, attrs) {
+      var inputFile = elem.parent().find('input')[0];
+      elem.bind('click', function() {
+        inputFile.click();
+      });
 
-			var inputId = attrs.inputId;
-			angular.element(inputFile).bind(
-					'change',
-					function() {
-						var inputText = angular.element(document
-								.getElementById(inputId));
-						inputText.val(angular.element(this).val());
-					});
+      var inputId = attrs.inputId;
+      angular.element(inputFile).bind(
+          'change',
+          function() {
+            var inputText = angular.element(document
+                .getElementById(inputId));
+            inputText.val(angular.element(this).val());
+          });
 
-		}
-	}
+    }
+  };
+} ]);
+
+directives.directive('tagSelection', [ 'tagService', function(tagService) {
+  return {
+    restrict : 'E',
+    templateUrl : 'templates/tags/tagSelection.html',
+    scope: {
+      book: '='
+    },
+    link : function(scope, elem, attrs) {
+
+      var book = scope.book;
+            
+      scope.tags = tagService.query();
+
+      scope.add = function(tag) {
+
+        if (tag == undefined || tag == '') {
+          alert('Choose a tag!');
+          return;
+        }
+
+        if (typeof book.tags == 'undefined') {
+          book.tags = [];
+        }
+
+        if (book.tags.indexOf(tag) != -1) {
+          alert("tag already added!");
+          return;
+        }
+
+        book.tags.push(tag);
+      };
+
+      scope.addNew = function(tagName) {
+
+        if (tagName == undefined || tagName == '') {
+          alert('tag must be not blank!');
+          return;
+        }
+
+        tagService.save({
+          'name' : tagName
+        }).$promise.then(function() {
+          scope.tags = tagService.query();
+          scope.tagName = '';
+        });
+      };
+
+      scope.remove = function(index) {
+        book.tags.splice(index, 1);
+      };
+
+    },
+    controllerAs : 'tagCtrl'
+  };
 } ]);
