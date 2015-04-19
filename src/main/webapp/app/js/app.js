9bb59ec4b64e849982a327c8c65a98de09481a74
landal79 <alex.landini@gmail.com>
2015-04-20 00:04:11 +0200

    added animations

--- src/main/webapp/app/js/app.js
+++ src/main/webapp/app/js/app.js
@@ -3,41 +3,41 @@
 //bookland main file
 
 var bookland = angular.module('bookland', [ 'bookland.services',
-		'bookland.directives', 'bookland.controllers', 'ngRoute', 'ngResource',
-		'ui.bootstrap' ]);
+    'bookland.directives', 'bookland.controllers', 'bookland.animations', 'ngRoute', 'ngResource',
+    'ui.bootstrap' ]);
 
 bookland.config(function($routeProvider) {
-	$routeProvider.when('/', {
-		templateUrl : 'views/default.html'
-	}).when('/list', {
-		templateUrl : 'views/books/list.html',
-		controller : 'ListController',
-		controllerAs : 'listCtrl'
-	}).when('/edit/:id', {
-		templateUrl : 'views/books/edit.html',
-		controller : 'BookController',
-		controllerAs : 'bookCtrl'
-	}).when('/new', {
-		templateUrl : 'views/books/edit.html',
-		controller : 'BookController',
-		controllerAs : 'bookCtrl'
-	}).when('/book/:id', {
-		templateUrl : 'views/books/detail.html',
-		controller : 'DetailController',
-		controllerAs : 'detailCtrl'
-	}).when('/newAuthor', {
-		templateUrl : 'views/authors/edit.html',
-		controller : 'NewAuthorController',
-		controllerAs : 'newAuthorCtrl'
-	}).when('/settings', {
-		templateUrl : 'views/settings.html',
-		controller : 'SettingsController',
-		controllerAs : 'settings'
-	}).when('/about', {
-		templateUrl : 'views/about.html',
-		controller : 'AboutController',
-		controllerAs : 'about'
-	}).otherwise({
-		redirectTo : '/'
-	});
+  $routeProvider.when('/', {
+    templateUrl : 'views/default.html'
+  }).when('/list', {
+    templateUrl : 'views/books/list.html',
+    controller : 'ListController',
+    controllerAs : 'listCtrl'
+  }).when('/edit/:id', {
+    templateUrl : 'views/books/edit.html',
+    controller : 'BookController',
+    controllerAs : 'bookCtrl'
+  }).when('/new', {
+    templateUrl : 'views/books/edit.html',
+    controller : 'BookController',
+    controllerAs : 'bookCtrl'
+  }).when('/book/:id', {
+    templateUrl : 'views/books/detail.html',
+    controller : 'DetailController',
+    controllerAs : 'detailCtrl'
+  }).when('/newAuthor', {
+    templateUrl : 'views/authors/edit.html',
+    controller : 'NewAuthorController',
+    controllerAs : 'newAuthorCtrl'
+  }).when('/settings', {
+    templateUrl : 'views/settings.html',
+    controller : 'SettingsController',
+    controllerAs : 'settings'
+  }).when('/about', {
+    templateUrl : 'views/about.html',
+    controller : 'AboutController',
+    controllerAs : 'about'
+  }).otherwise({
+    redirectTo : '/'
+  });
 });
