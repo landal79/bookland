587657f0cfe32b56f6ab50f26daee97d43e8221f
landal79 <alex.landini@gmail.com>
2017-05-17 18:11:57 +0200

    modified pom.xml

--- src/test/java/org/landal/bookland/repositories/TagRepositoryTest.java
+++ src/test/java/org/landal/bookland/repositories/TagRepositoryTest.java
@@ -12,8 +12,6 @@ import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.arquillian.persistence.ShouldMatchDataSet;
-import org.jboss.arquillian.persistence.TransactionMode;
-import org.jboss.arquillian.persistence.Transactional;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.asset.StringAsset;
@@ -24,7 +22,6 @@ import org.landal.bookland.model.Tag;
 import org.landal.bookland.utils.CoreDeployments;
 import org.landal.bookland.utils.PersistenceDeployments;
 
-@Transactional(TransactionMode.COMMIT)
 @RunWith(Arquillian.class)
 public class TagRepositoryTest {
 
@@ -46,18 +43,24 @@ public class TagRepositoryTest {
 
     @Test
     @ShouldMatchDataSet(value = { "tags.yml" }, excludeColumns = { "*id" })
-    public void test_persist() {
+    public void test_persist() throws Exception {
+
+        transaction.begin();
 
         Tag tag = new Tag("popular");
         tag = tagRepository.persist(tag);
 
         assertThat(tag.getId(), notNullValue());
 
+        transaction.commit();
+
     }
 
 
     @Test
     public void test_getAll() throws Exception {
+
+        transaction.begin();
 
         tagRepository.persist(new Tag("popular"));
         tagRepository.persist(new Tag("scifi"));
